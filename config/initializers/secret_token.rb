211057781cfe2f10a0ce8382f08f548c50fb3175
Sami Hamdi <sami.hamdi@perceptivesoftware.com>
2013-06-07 15:37:27 -0400

    Adding first_app files/directories

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '075fbf0afa98acf30f4cff737bb001a16f2d07cabb281247a450a8133186ead53d69bf7b7f85a6691e64bbdf633e33185422ecadbdd9101fe414bee205b046e7'
